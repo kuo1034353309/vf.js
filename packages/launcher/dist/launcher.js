075a4b3e7a8374412b4c03796ea1eb44dccfc01f
杨啸 <ifyx@qq.com>
2020-05-25 20:25:22 +0800

    v0.3.38 fix audio

--- packages/launcher/dist/launcher.js
+++ packages/launcher/dist/launcher.js
@@ -133,9 +133,9 @@ var VIPKIDLauncher = /** @class */ (function () {
         this._cdnsIndex = 0;
         this._errorLoadCount = 0;
         this._errorLoadMaxCount = 10;
-        this.version = "0.3.37";
+        this.version = "0.3.38";
         // eslint-disable-next-line no-undef
-        this.buildInfo = "2020-5-25 5:05:50 PM";
+        this.buildInfo = "2020-5-25 8:24:11 PM";
         this._extendsLibsUrl = [];
         this._loadcount = 0;
         this._loadMaxCount = 40;
@@ -219,7 +219,7 @@ var VIPKIDLauncher = /** @class */ (function () {
         if (this._config.showFPS) {
             libs.push(this.getLibUrl("https://s.vipkidstatic.com/vf/engine/debug/stats.min.js"));
         }
-        libs.push(this.getLibUrl("vf-v5.2.21-v16", cdn, 'vf'));
+        libs.push(this.getLibUrl("vf-v5.2.21-v17", cdn, 'vf'));
         extendsLibsUrl.forEach(function (value) {
             libs.push(_this.getLibUrl(value));
         });
@@ -316,7 +316,7 @@ var VIPKIDLauncher = /** @class */ (function () {
             script.setAttribute('name', 'vf-script');
             script.type = 'text/javascript';
             script.id = item.version;
-            script.title = "0.3.37";
+            script.title = "0.3.38";
             script.async = false;
             script.src = item.url;
             script.addEventListener('load', this.onJsComplete.bind(this), false);
@@ -394,7 +394,7 @@ var VIPKIDLauncher = /** @class */ (function () {
 }());
 function createVF(options, completeCall, errorCall) {
     var scripts = document.getElementsByName('vf-script');
-    var version = "0.3.37";
+    var version = "0.3.38";
     if (scripts.length > 0 && scripts[0].title !== version) {
         scripts.forEach(function (value) {
             if (value.parentNode) {
